44901329377271ae88c6f2ae9865b5ed5fa03617
Michael Francis <michael@melenion.com>
2025-07-26 12:07:15 -0400

    Needed to seperate out the impls

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use deku::{DekuRead, DekuReader, DekuWrite, DekuWriter};
+use deku::{
+    ctx::Endian, no_std_io, writer::Writer, DekuError, DekuRead, DekuReader, DekuWrite, DekuWriter,
+};
 
 #[derive(Debug, PartialEq, DekuRead, DekuWrite)]
 #[deku(endian = "endian", ctx = "endian: deku::ctx::Endian")]
@@ -13,19 +15,48 @@ pub struct CompactString {
     value: Vec<u8>,
 }
 
-#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
-#[deku(endian = "endian", ctx = "endian: deku::ctx::Endian")]
-
-pub struct CompactArray<T>
-where
-    T: for<'a> DekuReader<'a, deku::ctx::Endian>
-        + DekuWriter<deku::ctx::Endian>
-        + PartialEq
-        + std::fmt::Debug,
-{
+#[derive(Debug, PartialEq)]
+pub struct CompactArray<T> {
     length: VarInt,
-    #[deku(count = "length.0")]
     content: Vec<T>,
+}
+
+impl<'a, T> DekuReader<'a, Endian> for CompactArray<T>
+where
+    T: for<'b> DekuReader<'b, Endian>,
+{
+    fn from_reader_with_ctx<R: deku::no_std_io::Read + no_std_io::Seek>(
+        reader: &mut deku::reader::Reader<R>,
+        endian: deku::ctx::Endian,
+    ) -> Result<Self, DekuError>
+    where
+        Self: Sized,
+    {
+        let length = <VarInt as DekuReader<'_, _>>::from_reader_with_ctx(reader, endian)?;
+        let limit = deku::ctx::Limit::new_count(length.0 as usize);
+
+        let content =
+            <Vec<T> as DekuReader<'_, _>>::from_reader_with_ctx(reader, (limit, (endian)))?;
+
+        Ok(CompactArray { length, content })
+    }
+}
+
+impl<T> DekuWriter<Endian> for CompactArray<T>
+where
+    T: DekuWriter<Endian>,
+{
+    #[doc = " Write type to bytes"]
+    fn to_writer<W: no_std_io::Write + no_std_io::Seek>(
+        &self,
+        writer: &mut Writer<W>,
+        ctx: Endian,
+    ) -> Result<(), DekuError> {
+        self.content.to_writer(writer, ctx)?;
+        self.content.to_writer(writer, ctx)?;
+
+        Ok(())
+    }
 }
 
 #[derive(Debug, PartialEq, DekuRead)]
